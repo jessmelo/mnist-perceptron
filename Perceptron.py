1875e525e60a2490778bc322a60948729949f246
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-30 23:07:53 -0300

    updates

--- Perceptron.py
+++ Perceptron.py
@@ -1,30 +1,31 @@
 import numpy as np
 import random as rd
+import Datasets as ds 
 
-epocas = 50
+class Perceptron:
 
-# importando os datasets
-dir_path = "C:/Users/Jess/Documents/USP/2020.1/IA/Conjuntos MNIST/"
-treino = np.loadtxt(dir_path + '/mnist_treinamento.csv', dtype = float, delimiter = ',')
-teste = np.loadtxt(dir_path + '/mnist_teste.csv', dtype = float, delimiter = ',')
+	# definindo numero de epocas e bias 
+	self.epocas = 50
+	self.bias = 1.0 		
 
-# embaralhando os dados
+	# gerando os pesos aleatorios para as 785 entradas
+	def random_weights():
+		weights = np.zeros(785)
+		for i, j in enumerate(weights):
+			weights[i] = rd.uniform(-0.5, 0.5)
+		return weights
 
-# gerando os pesos aleatorios para as 785 entradas
-def random_weights():
-	weights = np.zeros(785)
-	for i, j in enumerate(weights):
-		weights[i] = rd.uniform(-0.5, 0.5)
-	return weights
+	print(random_weights())
 
-print(random_weights())
+	# soma ponderada das entradas
+	def net_input(amostras):
+		net = 0
+		pesos = random_weights()
+		for i in amostras.shape[0]:
+			for j in amostras:
+				net += pesos[j] * amostras[i][j]
+		return net
 
-# soma ponderada das entradas
-def net_input(amostras):
-	net = 0
-	pesos = random_weights()
-	for i in amostras[]:
-		for j in amostras:
-			net += pesos[j] * amostras[i][j]
-	return net
+
+	# serao treinados dez perceptrons um para cada digito
 
