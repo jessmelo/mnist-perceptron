caff019d1ddb5a5a5e85298c326b10c87c4a627c
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-31 17:33:47 -0300

    updates

--- Datasets.py
+++ Datasets.py
@@ -3,18 +3,18 @@ import numpy as np
 # importando os datasets
 
 dir_path = "C:/Users/Jess/Documents/USP/2020.1/IA/Conjuntos MNIST/"
-# treino = np.loadtxt(dir_path + '/mnist_treinamento.csv', dtype = float, delimiter = ',')
-# teste2 = np.loadtxt(dir_path + '/mnist_teste.csv', dtype = float, delimiter = ',')
+treino = np.loadtxt(dir_path + '/mnist_treinamento.csv', dtype = float, delimiter = ',')
+teste = np.loadtxt(dir_path + '/mnist_teste.csv', dtype = float, delimiter = ',')
 
 # usando o pickle para rodar mais rapido
 
 import pickle
 
-# with open("C:/Users/Jess/github/mnist-perceptron/pickled_mnist.pkl", "bw") as fh:
-# 	data = (
-#         treino, 
-#         teste)
-# 	pickle.dump(data, fh)
+with open("C:/Users/Jess/github/mnist-perceptron/pickled_mnist.pkl", "bw") as fh:
+	data = (
+        treino, 
+        teste)
+	pickle.dump(data, fh)
 
 with open("C:/Users/Jess/github/mnist-perceptron/pickled_mnist.pkl", "rb") as fh:
 	data = pickle.load(fh)
@@ -23,15 +23,15 @@ treino = data[0]
 teste = data[1]
 
 # embaralhando os dados
-from random import shuffle 
+# from random import shuffle 
 
-def shuffle_dataset(data):
-    shuffle_dataset = np.random.shuffle(data)
-    print(shuffle_dataset)
+# def shuffle_dataset(data):
+#     shuffle_dataset = np.random.shuffle(data)
+#     print(shuffle_dataset)
 
-print(teste)
+# print(teste)
 
-teste_embaralhado = teste  # make a copy
-shuffle_dataset = np.random.shuffle(teste_embaralhado)
-print("proximo")
-print(teste_embaralhado)
+# teste_embaralhado = teste  # make a copy
+# shuffle_dataset = np.random.shuffle(teste_embaralhado)
+# print("proximo")
+# print(teste_embaralhado)
--- Perceptron.py
+++ Perceptron.py
@@ -1,12 +1,39 @@
 import numpy as np
 import random as rd
-import Datasets as ds 
 
 class Perceptron:
 
 	# definindo numero de epocas e bias 
-	self.epocas = 50
-	self.bias = 1.0 		
+	epocas = 50
+	bias = 1.0 		
+
+	# importando os datasets com pickle
+	import pickle 
+	with open("C:/Users/Jess/github/mnist-perceptron/pickled_mnist.pkl", "rb") as fh:
+		data = pickle.load(fh)
+
+	treino = data[0]
+	teste = data[1]
+
+	# embaralhando os dados
+	def shuffle_dataset(data):
+		shuffle_dataset = data
+	    	shuffle_dataset = np.random.shuffle(data)
+	    	return shuffle_dataset
+
+	# print(teste)
+
+	# treino_embaralhado = teste  # make a copy
+	# np.random.shuffle(treino_embaralhado)
+	# print("treino embaralhado:")
+	# print(treino_embaralhado)
+
+	# print(treino)
+
+	# teste_embaralhado = teste  # make a copy
+	# np.random.shuffle(teste_embaralhado)
+	# print("teste embaralhado:")
+	# print(teste_embaralhado)
 
 	# gerando os pesos aleatorios para as 785 entradas
 	def random_weights():
@@ -18,14 +45,33 @@ class Perceptron:
 	print(random_weights())
 
 	# soma ponderada das entradas
-	def net_input(amostras):
+	def net_input(amostras, pesos):
 		net = 0
 		pesos = random_weights()
-		for i in amostras.shape[0]:
-			for j in amostras:
-				net += pesos[j] * amostras[i][j]
+		for i in range(amostras.shape[0]):
+			rotulo = amostras[i, 0]
+			for j in range(amostras.shape[1]-1):
+				net += pesos[j+1] * amostras[i][j+1]
 		return net
 
+	# confere resultado
+	def check_result(resultado_esperado, label_amostra):
+		if (resultado_esperado == label_amostra):
+			return 1
+		else:
+			return 0
+
+	# treina perceptron
+	def train_perceptron():
+
+		
+		taxa_aprendizado = 0.01 # taxa inicial
+		dados_embaralhados = shuffle_dataset(treino) # embaralhando os dados de treinamento
+
+		for i in range(epocas):			
+			pesos = random_weights() # gerando os pesos
+
+			soma_ponderada  = peso[0] * bias + net_input(dados_embaralhados, pesos)
 
 	# serao treinados dez perceptrons um para cada digito
 
